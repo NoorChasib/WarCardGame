3b42ce0b15785607270e761627c7234334639426
Noor Chasib <noorchasib@gmail.com>
2023-03-16 22:14:11 -0700

    init commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Noor Chasib on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
