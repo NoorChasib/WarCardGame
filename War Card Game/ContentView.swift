87d85c13abc7f2e7badd7eb691745cd154679cbc
Noor Chasib <noorchasib@gmail.com>
2023-03-17 14:24:14 -0700

    create logic for adding score and randomizing cards

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-   var playerCard = "card7"
-   var cpuCard = "card13"
-   var playerScore = 0
-   var cpuScore = 0
+   @State var playerCard = "back"
+   @State var cpuCard = "back"
+   @State var playerScore = 0
+   @State var cpuScore = 0
 
    var body: some View {
       ZStack {
@@ -66,7 +66,23 @@ struct ContentView: View {
    }
 
    func deal() {
-      print("Deal Cards")
+      // Randomize the player card
+      var playerCardValue = Int.random(in: 2...14)
+      playerCard = "card" + String(playerCardValue)
+
+      // Randomize the cpu card
+      var cpuCardValue = Int.random(in: 2...14)
+      cpuCard = "card" + String(cpuCardValue)
+
+      // Update the score
+      if playerCardValue > cpuCardValue {
+         // Add 1 to player score
+         playerScore += 1
+      }
+      else if cpuCardValue > playerCardValue {
+         // Add 1 to cpu score
+         cpuScore += 1
+      }
    }
 }
 
